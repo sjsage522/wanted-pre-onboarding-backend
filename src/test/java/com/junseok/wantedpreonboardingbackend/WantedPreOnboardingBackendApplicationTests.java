54b1388c14a2c560e7ff1eeb3c05a0f2f93cb33a
Junseok Shin <sjsage522@gmail.com>
2023-08-04 09:13:31 +0900

    add: project init

---
+++ src/test/java/com/junseok/wantedpreonboardingbackend/WantedPreOnboardingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junseok.wantedpreonboardingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WantedPreOnboardingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
