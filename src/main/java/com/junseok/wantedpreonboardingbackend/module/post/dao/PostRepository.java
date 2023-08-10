38212728259dc94ec02a38b967ec175b69db6754
Junseok Shin <sjsage522@gmail.com>
2023-08-10 11:20:27 +0900

    refactor : 게시글 조회 서비스 리펙터링 #29
    OSIV defalut 설정으로 인한 db conn 유지
    - OSIV -> false 설정
    - 조회로직에서 지연로딩(user) 부분
      - @transactional(readOnly = true) 로 conn 범위 살리기
        - 변경 감지를 위한 Snapshot을 따로 보관하지 않으므로 메모리가 절약
      - post 조회 시 join 쿼리 적용

--- src/main/java/com/junseok/wantedpreonboardingbackend/module/post/dao/PostRepository.java
+++ src/main/java/com/junseok/wantedpreonboardingbackend/module/post/dao/PostRepository.java
@@ -1,7 +1,20 @@
 package com.junseok.wantedpreonboardingbackend.module.post.dao;
 
 import com.junseok.wantedpreonboardingbackend.module.post.domain.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    @EntityGraph(attributePaths = {"user"})
+    @Override
+    Page<Post> findAll(Pageable pageable);
+
+    @EntityGraph(attributePaths = {"user"})
+    @Override
+    Optional<Post> findById(Long id);
 }
